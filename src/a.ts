32441976a96a40ffca850b7978ea83d3fb23a09f
Pawel Badenski <pawel.badenski@gmail.com>
2017-10-27 09:55:16 +0100

    generic type on a method

--- src/a.ts
+++ src/a.ts
@@ -1,13 +1,1 @@
-export {};
-
-const o = <any> { a: 2 };
-
-const f = () => (<any> {
-  property: "value"
-});
-
-const f2 = () => {
-  return <any> {
-    property: "value"
-  };
-};
+const foo = <T, V> (x: X<T, V>): (f: Function) => null;

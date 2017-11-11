61069241574766e5c51e19df75be0cb13fc4b960
Pawel Badenski <pawel.badenski@gmail.com>
2017-11-11 10:57:38 -0600

    fix non-compiling example

--- src/a.ts
+++ src/a.ts
@@ -1,1 +1,4 @@
-const foo = <T, V> (x: X<T, V>): (f: Function) => null;
+interface X<A> {}
+interface F<A, B, C> {}
+export const liftA2 = <T, V> (fa: X<T>, fb: X<V>): <U>(f: F<T, V, U>) => X<U> =>
+  f => null;
